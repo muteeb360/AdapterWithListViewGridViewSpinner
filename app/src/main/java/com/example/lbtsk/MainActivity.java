0c8fd88d0bcdb5f659571cd484115a82abba8dfa
muteeb360 <muteebbutt1819@gmail.com>
2023-05-03 23:39:06 +0500

    set up grid view and spinner

--- app/src/main/java/com/example/lbtsk/MainActivity.java
+++ app/src/main/java/com/example/lbtsk/MainActivity.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.GridView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,8 +15,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] months = {"Jan","feb","march","april","may"};
+
+        //---------->setting default adapter
+
+        //fetched adapter views from layout file
         ListView listView = findViewById(R.id.list);
+        GridView grd = findViewById(R.id.grid);
+        Spinner spn = findViewById(R.id.spin);
+
+        //initialized adapters
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.data));
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, months);
+        ArrayAdapter<String> adapter3 = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, months);
+
+        //set up adapters with respective adapter views
         listView.setAdapter(adapter);
+        grd.setAdapter(adapter2);
+        spn.setAdapter(adapter3);
+
+
     }
 }
